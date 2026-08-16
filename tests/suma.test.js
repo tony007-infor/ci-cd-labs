1cc5e83aa17d55b3d2c05b24803ff5a016f1f005
tony007-infor <tu.email@ejemplo.com>
2026-08-16 16:17:03 -0400

    test: provocar fallo intencional en una prueba unitaria

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -1,7 +1,7 @@
 const suma = require('../src/suma');
 
 test('suma 2 + 3 debe ser 5', () => {
-  expect(suma(2, 3)).toBe(5);
+  expect(suma(2, 3)).toBe(99); // 👈 valor incorrecto a propósito
 });
 
 test('suma -1 + 1 debe ser 0', () => {
